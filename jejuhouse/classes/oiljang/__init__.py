339ae8f527daa103eaa23b339fce96def7b3ea09
skycream <9gompig@naver.com>
2025-01-12 17:10:03 +0900

    오일장

--- jejuhouse/classes/oiljang/__init__.py
+++ jejuhouse/classes/oiljang/__init__.py
@@ -3,7 +3,7 @@
 class OilJang:
     def __init__(self):
         self.name = '오일장'
-        self.url = 'https://www.jejuall.com/CProperty'
+        self.url = 'https://www.jejuall.com/CProperty/detail?num='
     
     from classes.oiljang.get import get_data
     from classes.oiljang.set import set_data
--- jejuhouse/classes/oiljang/get.py
+++ jejuhouse/classes/oiljang/get.py
@@ -1,4 +1,7 @@
+import json
+import re
 import requests
+import bs4
 
 def get_data(self):
     a = clawer(self)
@@ -7,8 +10,71 @@ def get_data(self):
 
 
 def clawer(self):
-    cur_num = 4406936
+    cur_num = '4406936'
     res = requests.get(self.url + cur_num)
-    print(res)
+    res.encoding = 'utf-8'
+    html = res.text
+    bs = bs4.BeautifulSoup(html, 'html.parser')
+    rows = bs.select("table.info_t tr")
+    print(rows)
+    table_json_data = set_table_data(rows)
+    print(table_json_data)
+
+
+def set_table_data(table_html_data):
+    data = {}
     
-    pass
+    # 데이터 추출용 함수: <br>로 구분된 텍스트를 배열로 처리하고 불필요한 태그 및 공백 제거
+    def get_text_with_br_as_list(td):
+        # <br> 태그를 기준으로 텍스트 분리 후 HTML 태그 및 공백 제거
+        parts = re.split(r'<br\s*/?>', td.decode_contents())
+        parts = [re.sub(r'</?br>', '', text).strip() for text in parts if text.strip()]  # 태그 제거
+        if len(parts) > 1:
+            return parts  # 배열로 반환
+        return parts[0] if parts else ""  # 단일 값 반환
+    
+    # 텍스트 정리: \n, \t, 공백 제거 및 괄호 처리
+    def clean_text(value):
+        value = re.sub(r'[\n\t]+', '', value)  # \n, \t 제거
+        value = re.sub(r'\s+', ' ', value)  # 중복 공백 제거
+        return value.strip()
+    
+    # 괄호 속 데이터 분리 (특정 필드용)
+    def split_parens(value):
+        value = clean_text(value)  # 사전에 텍스트 정리
+        match = re.match(r'(.+)\((.+)\)', value)
+        if match:
+            return match.group(1).strip(), match.group(2).strip()  # 괄호 밖과 안 데이터 분리
+        return value, None  # 괄호가 없을 경우 그대로 반환
+
+    for row in table_html_data:
+        cols = row.find_all("td")
+        
+        if len(cols) == 2:  # 제목과 내용이 1:1 매핑된 경우
+            key = clean_text(cols[0].get_text(strip=True))
+            value = get_text_with_br_as_list(cols[1])
+            data[key] = value
+
+        elif len(cols) == 4:  # 제목과 내용이 2개씩 있는 경우
+            key1 = clean_text(cols[0].get_text(strip=True))
+            value1 = get_text_with_br_as_list(cols[1])
+            key2 = clean_text(cols[2].get_text(strip=True))
+            value2 = get_text_with_br_as_list(cols[3])
+
+            # "총주차대수"와 같은 경우 괄호 처리
+            if key1 == "총주차대수":
+                main_value, extra_value = split_parens(value1)
+                data[key1] = {"주차대수": main_value, "비율": extra_value}
+            else:
+                data[key1] = value1
+
+            if key2 == "총주차대수":
+                main_value, extra_value = split_parens(value2)
+                data[key2] = {"주차대수": main_value, "비율": extra_value}
+            else:
+                data[key2] = value2
+
+    # JSON 형식으로 변환
+    json_data = json.dumps(data, ensure_ascii=False, indent=4)
+    return json_data
+    
