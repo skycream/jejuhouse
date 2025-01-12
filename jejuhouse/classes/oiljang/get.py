7c4e78e3fac37157312dfa78aaee01dd2e87fd22
skycream <9gompig@naver.com>
2025-01-12 17:38:40 +0900

    오일장

--- jejuhouse/classes/oiljang/get.py
+++ jejuhouse/classes/oiljang/get.py
@@ -5,8 +5,6 @@ import bs4
 
 def get_data(self):
     a = clawer(self)
-    pass
-
 
 
 def clawer(self):
@@ -16,9 +14,18 @@ def clawer(self):
     html = res.text
     bs = bs4.BeautifulSoup(html, 'html.parser')
     rows = bs.select("table.info_t tr")
-    print(rows)
-    table_json_data = set_table_data(rows)
-    print(table_json_data)
+    table_data = set_table_data(rows)
+
+    # detail_data 처리: Tag 객체를 문자열로 변환
+    detail_data = bs.select_one("div.detail_text_cont")
+    if detail_data:
+        table_data['상세정보'] = detail_data.decode_contents().strip()  # 문자열로 변환
+    else:
+        table_data['상세정보'] = None
+
+    total_data = table_data
+    json_data = json.dumps(total_data, ensure_ascii=False, indent=4)
+    print(json_data)
 
 
 def set_table_data(table_html_data):
@@ -26,6 +33,8 @@ def set_table_data(table_html_data):
     
     # 데이터 추출용 함수: <br>로 구분된 텍스트를 배열로 처리하고 불필요한 태그 및 공백 제거
     def get_text_with_br_as_list(td):
+        if isinstance(td, str):  # 이미 문자열인 경우
+            return td.strip()
         # <br> 태그를 기준으로 텍스트 분리 후 HTML 태그 및 공백 제거
         parts = re.split(r'<br\s*/?>', td.decode_contents())
         parts = [re.sub(r'</?br>', '', text).strip() for text in parts if text.strip()]  # 태그 제거
@@ -35,6 +44,8 @@ def set_table_data(table_html_data):
     
     # 텍스트 정리: \n, \t, 공백 제거 및 괄호 처리
     def clean_text(value):
+        if not isinstance(value, str):  # 문자열이 아닐 경우 처리
+            value = value.get_text(strip=True) if value else ""
         value = re.sub(r'[\n\t]+', '', value)  # \n, \t 제거
         value = re.sub(r'\s+', ' ', value)  # 중복 공백 제거
         return value.strip()
@@ -51,14 +62,14 @@ def set_table_data(table_html_data):
         cols = row.find_all("td")
         
         if len(cols) == 2:  # 제목과 내용이 1:1 매핑된 경우
-            key = clean_text(cols[0].get_text(strip=True))
+            key = clean_text(cols[0])
             value = get_text_with_br_as_list(cols[1])
             data[key] = value
 
         elif len(cols) == 4:  # 제목과 내용이 2개씩 있는 경우
-            key1 = clean_text(cols[0].get_text(strip=True))
+            key1 = clean_text(cols[0])
             value1 = get_text_with_br_as_list(cols[1])
-            key2 = clean_text(cols[2].get_text(strip=True))
+            key2 = clean_text(cols[2])
             value2 = get_text_with_br_as_list(cols[3])
 
             # "총주차대수"와 같은 경우 괄호 처리
@@ -74,7 +85,4 @@ def set_table_data(table_html_data):
             else:
                 data[key2] = value2
 
-    # JSON 형식으로 변환
-    json_data = json.dumps(data, ensure_ascii=False, indent=4)
-    return json_data
-    
+    return data
