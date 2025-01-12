e8ad73fab38f1180497e07fabebd87ec2d6890fa
skycream <9gompig@naver.com>
2025-01-12 15:09:28 +0900

    init

---
+++ jejuhouse/kyocharo/__init__.py
@@ -0,0 +1,12 @@
+
+
+class Kyocharo:
+    def __init__(self):
+        self.name = '교차로'
+    
+    from kyocharo.get import get_data
+    from kyocharo.set import set_data
+    from kyocharo.export import export_data
+
+    
+    
---
+++ jejuhouse/kyocharo/export.py
@@ -0,0 +1,4 @@
+
+def export_data():
+    pass
+
