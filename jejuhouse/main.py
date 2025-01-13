84c6857caac1361792dc48fa4e61be324a7e7ed4
Cullen <cullen@Cullenui-Mac-Studio.local>
2025-01-13 17:05:29 +0900

    Modify

--- jejuhouse/main.py
+++ jejuhouse/main.py
@@ -1,6 +1,13 @@
-
+from classes.auction import Auction
+from classes.blog import NaverBlog
 from classes.oiljang import OilJang
 from classes.kyocharo import Kyocharo
+
+a = Auction()
+a_data = a.get_data()
+
+b = NaverBlog()
+b_data = b.get_data()
 
 o = OilJang()
 o_data = o.get_data()
@@ -8,7 +15,11 @@ o_data = o.get_data()
 k = Kyocharo()
 k_data = k.get_data()
 
-data = {'오일장': o_data, '교차로': k_data}
+
+
+data = {'온비드' : a_data, '네이버블로그' : b_data, '오일장': o_data, '교차로': k_data}
 print(data)
+print(len(data['온비드']))
+print(len(data['네이버블로그']))
 print(len(data['오일장']))
 print(len(data['교차로']))
